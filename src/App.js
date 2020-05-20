a11ff06abda05c2ba03daecf407c38c6f511eadf
nkosi-tauro <nkosilati23@yahoo.com>
2020-05-21 00:49:43 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App = props => {
+  const [state, setState] = useState({
+    selectedCharacter: 1,
+    side : "light",
+    destroyed: false
+  });
+
+  const sidehandler = side => {
+    setState(
+      { ...state,side: side }
+    )
+  }
+
+  const charSelectHandler = event => {
+    const charId = event.target.value;
+    setState({...state,selectedCharacter: charId})
+  }
+
+  const destructionHandler = () => {
+    setState(
+      { ...state,destroyed : true }
+    )
+  }
+
+  let content = (
+    <React.Fragment>
+      <CharPicker
+        side={state.side}
+        selectedChar={state.selectedCharacter}
+        onCharSelect={state.charSelectHandler}
+      />
+      <Character selectedChar={state.selectedCharacter}/>
+      <button onClick={sidehandler.bind(this,"light")}>Light Side</button>
+      <button onClick={sidehandler.bind(this,"dark")}>Dark Side</button>
+      {state.side === "dark" && (
+        <button onClick={destructionHandler}>Destroy!</button>
+      )}
+    </React.Fragment>
   );
-}
+
+  if (state.destroyed){
+    content= (<h1>Total Destruction</h1>)
+  }
+
+  return content;
+
+};
 
 export default App;
