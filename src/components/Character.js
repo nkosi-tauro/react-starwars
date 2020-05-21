0c640a6eee0233547f995049133cc8db598f25df
nkosi-tauro <nkosilati23@yahoo.com>
2020-05-21 02:01:55 +0200

    updated Characters

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,8 +1,68 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Summary from './Summary';
 
 
 
-const Character = props => {}
+const Character = props => {
+
+    const [loadedCharacter, setLoadedCharacter] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
+
+
+
+    const fetchData = () => {
+        setIsLoading(true)
+        fetch(`https://swapi.co/api/people${props.selectedChar}`)
+        .then(data => {
+            if (!data.ok){
+                throw new Error ("Failed to fetch Person")
+            }
+            return data;
+        })
+        .then(charData => {
+            const loadedCharacter = {
+                id: props.selectedChar,
+                name : charData.name,
+                height: charData.height,
+                colors : {
+                    hair : charData.hair_color,
+                    skin : charData.skin_color
+                },
+                gender : charData.gender,
+                movieCount: charData.films.length
+            };
+            setIsLoading(false)
+            setLoadedCharacter(loadedCharacter)
+        })
+        .catch(err => {
+            console.log(err)
+            setIsLoading(false)
+        })
+    }
+
+    useEffect(() => {
+        fetchData();
+    },[props.selectedChar])
+
+    let content = <p>Loading Character...</p>
+
+    if (!isLoading && loadedCharacter.id){
+        content = (
+            <Summary
+                name={loadedCharacter.name}
+                gender={loadedCharacter.gender}
+                height={loadedCharacter.height}
+                hairColor={loadedCharacter.color.hair}
+                skinColor={loadedCharacter.color.skin}
+                movieCount={loadedCharacter.movieCount}
+            />
+        )
+    }else if (!isLoading && ! loadedCharacter.id){
+        content = <p> Failed to fetch character</p>
+    }
+
+    return content;
+}
 
 
 
